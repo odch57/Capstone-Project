c5440915263bfcd18a94e2c17a870ed8cdafce72
Robin Chen <robin3chen@gmail.com>
2016-08-02 15:26:55 -0700

    Content Provider changed
    Provider made to be exportable for other (transit) apps to use.

--- app/src/main/java/com/robsterthelobster/ucibustracker/data/db/BusContract.java
+++ app/src/main/java/com/robsterthelobster/ucibustracker/data/db/BusContract.java
@@ -9,7 +9,7 @@ import android.provider.BaseColumns;
  * Created by robin on 6/21/2016.
  */
 public class BusContract {
-    public static final String CONTENT_AUTHORITY = "com.robsterthelobster.ucibustracker";
+    public static final String CONTENT_AUTHORITY = "com.robsterthelobster.bustracker";
 
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
